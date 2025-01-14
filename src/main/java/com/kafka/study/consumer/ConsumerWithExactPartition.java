6cdd333d43ee63b3c70566775f1f6630c99dab99
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-01-14 19:21:50 +0900

    consumer : 파티션 할당 컨슈머

--- src/main/java/com/kafka/study/consumer/ConsumerWithExactPartition.java
+++ src/main/java/com/kafka/study/consumer/ConsumerWithExactPartition.java
@@ -5,33 +5,34 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
-public class ConsumerMain {
+// 파티션 할당 컨슈머
+// 그룹ID 필수 X, 흔히 사용되는 방식은 아님
+public class ConsumerWithExactPartition {
 
     private final static Logger logger = LoggerFactory.getLogger(ProducerWithSyncCallbackMain.class);
     private final static String TOPIC_NAME = "test";
     private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
-    private final static String GROUP_ID = "test_group";
+    private final static int PARTITION_NUMBER = 0;
 
-    // consumer 기본 로직(자동 커밋)
     public static void main(String[] args) {
 
         Properties configs = new Properties();
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
-
-        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+        consumer.assign(Collections.singleton(new TopicPartition(TOPIC_NAME, PARTITION_NUMBER))); // 파티션 번호 지정
 
         while(true){
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
--- src/main/java/com/kafka/study/consumer/ConsumerWithRebalanceListener.java
+++ src/main/java/com/kafka/study/consumer/ConsumerWithRebalanceListener.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.Properties;
 
 // 리밸런스 리스너를 가진 컨슈머
+// EditConfigurations -> modify options -> Allow multiple instance
 public class ConsumerWithRebalanceListener {
 
     private final static Logger logger = LoggerFactory.getLogger(ProducerWithSyncCallbackMain.class);
